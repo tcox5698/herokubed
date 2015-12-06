3517d4e1d52dcdc3aa8b9c7858c38c90c826abba
tcox5698 <tcox56_98@yahoo.com>
2015-12-06 13:39:10 -0600

    cucumber logs into heroku

--- features/step_definitions/herokubed_steps.rb
+++ features/step_definitions/herokubed_steps.rb
@@ -1,7 +1,33 @@
-Given(/^things$/) do
-  puts 'things'
+require 'open3'
+require 'pty'
+
+def flush_input_out(output)
+  begin
+    sofar = output.read_nonblock 1024
+    error = "stdin received empty string. Set env variables for HEROKU_USER_NAME and HEROKU_PASSWORD."
+    expect(sofar.chomp.strip).not_to be_empty, error
+  rescue => e
+    puts "exception reading output: #{e}"
+  end
 end
 
-Then(/^other things$/) do
-  puts 'other things'
+def flush_input(input_text, input_stream, output_stream)
+  input_stream.print(input_text)
+  flush_input_out(output_stream)
+end
+
+Given(/^I have logged into heroku$/) do
+  expect(`heroku auth:logout`).to include 'cleared'
+
+  PTY.spawn('heroku auth:login') do |output, input, pid|
+    flush_input( "#{ENV['HEROKU_USER_NAME']}\n", input, output)
+    flush_input("#{ENV['HEROKU_PASSWORD']}\n", input, output)
+    input.close
+
+    output.each_line do |line|
+      expect(line).not_to match /failure/
+    end
+  end
+
+  expect(`heroku auth:token`.chomp.size).to eq 36
 end
--- features/transfer_database.feature
+++ features/transfer_database.feature
@@ -1,5 +1,9 @@
 Feature: I can transfer a postgres database from one app instance to another.
 
   Scenario: I can transfer a db from one existing instance to another existing instance.
-    Given things
-    Then other things
+    Given I have logged into heroku
+#    And I have app 'kubedApp1' with a postgres database
+#    And I add table 'kubedTable1' to app 'kubedApp1'
+#    And I have app 'kubedApp2' with a postgres database
+#    When I execute 'k transferdb kubedApp1 kubedApp2'
+#    Then app 'kubedApp2' has a table 'kubedTable1'
