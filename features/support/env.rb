112e0fbc344321ce8207c1cfca547c192dbf74d6
tcox5698 <tcox56_98@yahoo.com>
2015-12-10 18:34:29 -0600

    cucumber can create and destroy app - move it to herokubed

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,3 +1,23 @@
+def test_app_names
+  @test_app_names ||= []
+end
+
+def create_test_app test_app_name
+  puts "creating app #{test_app_name}"
+  test_app_names << test_app_name
+  curl_command = %Q( curl -X POST https://api.heroku.com/apps -H "Accept: application/vnd.heroku+json; version=3" -H "Authorization: Bearer $HEROKU_TOKEN" -H "Content-Type: application/json"  -d '{"name": "#{test_app_name}"}' )
+  puts "curl command: #{curl_command}"
+  expect(`#{curl_command}`.chomp).to include test_app_name
+  puts 'done creating app'
+end
+
 Before do
   puts 'IM BEFORE'
+end
+
+After do
+  test_app_names.each do |test_app_name|
+    puts "deleted app: #{test_app_name}"
+    expect(`heroku apps:destroy -a #{test_app_name} --confirm #{test_app_name}`.chomp).to eq ''
+  end
 end
--- features/transfer_database.feature
+++ features/transfer_database.feature
@@ -2,7 +2,7 @@ Feature: I can transfer a postgres database from one app instance to another.
 
   Scenario: I can transfer a db from one existing instance to another existing instance.
     Given I have logged into heroku
-#    And I have app 'kubedApp1' with a postgres database
+    And I have app 'kubedapp1' with a postgres database
 #    And I add table 'kubedTable1' to app 'kubedApp1'
 #    And I have app 'kubedApp2' with a postgres database
 #    When I execute 'k transferdb kubedApp1 kubedApp2'
