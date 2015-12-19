8f00c1064df7463e9a1c6f0046036798ce76df95
tcox5698 <tcox56_98@yahoo.com>
2015-12-19 07:13:07 -0600

    some change from last week is working

--- features/step_definitions/herokubed_steps.rb
+++ features/step_definitions/herokubed_steps.rb
@@ -12,12 +12,17 @@ And(/^I have app '(.*)' with a postgres database$/) do |app_name|
   add_postgres_addon(env_app_name(app_name))
 end
 
-And(/^I add table '(.*)' to app '(.*)'$/) do |table_name, app_name|
+And(/^I add table '(.*)' to app '(.*)' with the following records$/) do |table_name, app_name, table|
+  columns = table.hashes.first.keys
+  columns_strings = columns.map{|original| "#{original} integer"}
+  columns_string = columns_strings.join(', ')
+
   database_url = JSON.parse(call_heroku('GET', "apps/#{env_app_name(app_name)}/config-vars"))['DATABASE_URL']
   conn = PG.connect(database_url)
-  conn.exec("CREATE TABLE #{table_name} (id integer);")
+  conn.exec("CREATE TABLE #{table_name} (#{columns_string});")
 
-  conn.exec("INSERT INTO #{table_name} values (1234);")
+
+  conn.exec("INSERT INTO #{table_name} (#{columns.join(',')}) values (1234);")
 
   conn.exec("SELECT * FROM #{table_name}") do |result|
     result.each do |row|
--- features/transfer_database.feature
+++ features/transfer_database.feature
@@ -3,7 +3,9 @@ Feature: I can transfer a postgres database from one app instance to another.
   Scenario: I can transfer a db from one existing instance to another existing instance.
     Given I have logged into heroku
     And I have app 'kubedapp1' with a postgres database
-    And I add table 'kubedTable1' to app 'kubedapp1'
-#    And I have app 'kubedapp1' with a postgres database
-#    When I execute 'k transferdb kubedapp1 kubedapp1'
-#    Then app 'kubedapp1' has a table 'kubedTable1'
+    And I add table 'kubedTable1' to app 'kubedapp1' with the following records
+      | id   |
+      | 4567 |
+    And I have app 'kubedapp2' with a postgres database
+#    When I execute 'k transferdb kubedapp1 kubedapp2'
+#    Then app 'kubedapp2' has a table 'kubedTable1'
